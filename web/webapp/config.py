7e5473a15e69d8ac4859598e5cfe5126ca6ec8ed
pekshevds <pekshev.ds@gmail.com>
2022-10-11 09:21:04 +0300

    20221011_001

--- web/webapp/config.py
+++ web/webapp/config.py
@@ -8,3 +8,5 @@ WEATHER_DEFAULT_CITY = "Moscow,Russia"
 WEATHER_API_KEY = "3ee7edd3364d4635a6055621220410"
 WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'webapp.db')
+
+SECRET_KEY = "ecovijelcmelxcvemwxweilhrwmiehlrxeiobrxce;povciwe"
